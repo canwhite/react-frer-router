b6d549f2fa344bdd9a23072f59ca1c1b542b1408
qiaochao <752716886@qq.com>
2021-09-15 09:58:58 +0800

    for

--- src/components/InfoDetails.js
+++ src/components/InfoDetails.js
@@ -4,7 +4,8 @@ import {Link} from "react-router-dom"
 function InfoDetails(props){
 
     const [data,setData] = useState("");
-    const [num,setNum] = useState(0);
+    const [num,setNum] = useState(0);   
+    const [list,setList] = useState(["1","2","3"]);
 
     //接收传递过来的值
     useEffect(()=>{
@@ -24,6 +25,13 @@ function InfoDetails(props){
             <div>Details</div>
             <div>query:{data}</div>
             <div>frer:{num}</div>
+            {/* for循环添加标签注意用map和返回新标签 */}
+            <div> 
+                {list.map((item,index)=>{
+                    return(<p>{item}</p>) 
+                })}
+            </div>
+
             <div> 
                 
                     <Link to={{pathname:"/home",query:{
